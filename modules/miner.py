d84addd61805a4f7bfa35120c557336fbc985037
radistoubalidis <rtoumpalidis@gmail.com>
2023-12-02 02:19:08 +0200

    better miner

--- modules/miner.py
+++ modules/miner.py
@@ -5,7 +5,7 @@ def get_fix_commits(dataset_res: dict) -> pd.DataFrame:
     for item in dataset_res['rows']:
         dataset_list.append(item['row'])
     df = pd.DataFrame(dataset_list)
-    patterns = 'fix|bug|patch|repair|refactor|address'
+    patterns = 'fix|bug|patch|repair|refactor|address|error|conflict|revert'
     buggy_rows =  df[df['message'].str.contains(patterns, case=False, na=False)]
     only_code = buggy_rows[['old_contents','new_contents']].copy()
     return only_code
