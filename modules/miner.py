b98c9276febc7d1e546d36a30f735a9a0d88d0fa
radistoubalidis <rtoumpalidis@gmail.com>
2023-12-17 17:46:20 +0200

    get filtered dataset

--- modules/miner.py
+++ modules/miner.py
@@ -1,12 +1,10 @@
+import json
+import os
 import pandas as pd
+import sqlite3
 
-def get_fix_commits(dataset_res: dict, dataset_res2: dict = None) -> pd.DataFrame:
-    dataset_list = []
-    for item in dataset_res['rows']:
-        dataset_list.append(item['row'])
-    for item in dataset_res2['rows']:
-        dataset_list.append(item['row'])
-    df = pd.DataFrame(dataset_list)
+
+def get_dataset_filtered_commits() -> pd.DataFrame:
     patterns_EN = ['fix','bug','patch','repair','refactor','address','error','conflict','revert']
     patterns_CN = ['修正','错误','补丁','修理','重构','地址','错误','冲突','还原']
     patterns_RU = ['исправить', 'ошибка', 'исправление', 'исправить', 'рефакторинг', 'адрес', 'ошибка', 'конфликт', 'отменить']
@@ -15,7 +13,84 @@ def get_fix_commits(dataset_res: dict, dataset_res2: dict = None) -> pd.DataFram
     patterns_FR = ['corriger','erreur','pilote','réparer','refactoriser','adresse','erreur','conflit','inverser']
     patterns_DE = ['korrigieren','Fehler','Patch','reparieren','refaktorisieren','Adresse','Fehler','Konflikt','zurücksetzen']
     patterns = patterns_EN + patterns_CN + patterns_RU + patterns_JP + patterns_SP + patterns_FR + patterns_DE
-    buggy_rows =  df[df['message'].str.contains('|'.join(patterns), case=False, na=False)]
-    only_code = buggy_rows[['old_contents','new_contents']].copy()
-    return only_code
+
+    query = """
+    SELECT * FROM commitpackft
+    WHERE message like '%fix%'
+    OR message like '%bug%'
+    OR message like '%patch%'
+    OR message like '%repair%'
+    OR message like '%refactor%'
+    OR message like '%address%'
+    OR message like '%error%'
+    OR message like '%conflict%'
+    OR message like '%revert%'
+    OR message like '%修正%'
+    OR message like '%错误%'
+    OR message like '%补丁%'
+    OR message like '%修理%'
+    OR message like '%重构%'
+    OR message like '%地址%'
+    OR message like '%错误%'
+    OR message like '%冲突%'
+    OR message like'%还原%'
+    OR message like '%исправить%'
+    OR message like'%ошибка%'
+    OR message like '%исправление%'
+    OR message like'%исправить%'
+    OR message like '%рефакторинг%'
+    OR message like'%адрес%'
+    OR message like '%ошибка%'
+    OR message like'%конфликт%'
+    OR message like '%отменить%'
+    OR message like '%修正する%'
+    OR message like '%バグ%'
+    OR message like'%パッチ%'
+    OR message like '%修理する%'
+    OR message like'%リファクタリング%'
+    OR message like '%アドレス%'
+    OR message like'%エラー%'
+    OR message like '%競合%'
+    OR message like'%復元する%'
+    OR message like '%arreglar%'
+    OR message like'%error%'
+    OR message like'%parche%'
+    OR message like '%reparar%'
+    OR message like '%refactorizar%'
+    OR message like '%direccionar%'
+    OR message like '%error%'
+    OR message like '%conflicto%'
+    OR message like'%revertir%'
+    OR message like'%corriger%'
+    OR message like'%erreur%'
+    OR message like '%pilote%'
+    OR message like '%réparer%'
+    OR message like '%refactoriser%'
+    OR message like '%adresse%'
+    OR message like'%erreur%'
+    OR message like '%conflit%'
+    OR message like '%inverser%'
+    OR message like '%korrigieren%'
+    OR message like '%Fehler%'
+    OR message like'%Patch%'
+    OR message like '%reparieren%'
+    OR message like '%refaktorisieren%'
+    OR message like '%Adresse%'
+    OR message like '%Fehler%'
+    OR message like '%Konflikt%'
+    """
+
+    con = sqlite3.connect('commipack_datasets.sql')
+    dataset_df = pd.read_sql_query(query, con)
+    return dataset_df
+
+def writeJS(path: str, contents: str):
+    if os.path.exists(path):
+        os.remove(path)
+    with open(path,'w') as f:
+        f.write(contents)
         
+def filter_eslint(output: dict):
+    output_new = output[0]['messages']
+    output_old = output[1]['messages']
+    return 1 if len(output_old) < len(output_new) else 0
