22f165a71982350792f333fca8182f6d7d634743
radistoubalidis <rtoumpalidis@gmail.com>
2024-09-19 20:31:06 +0300

    updates to bert

--- modules/models.py
+++ modules/models.py
@@ -7,13 +7,16 @@ from transformers import (
     RobertaForMaskedLM,
     T5ForConditionalGeneration,
     T5Config,
+    RobertaTokenizer
     )
 import pytorch_lightning as pl
 import torch.optim as optim
 import torch.nn as nn
+from datetime import datetime
+from difflib import unified_diff
 
 class CodeBertJS(pl.LightningModule):
-    def __init__(self) -> None:
+    def __init__(self, tokenizer: RobertaTokenizer) -> None:
         super().__init__()
         self.encoder = RobertaForMaskedLM.from_pretrained(
                     'microsoft/codebert-base-mlm',
@@ -24,6 +27,9 @@ class CodeBertJS(pl.LightningModule):
                     return_dict=True,
                 )
         self.criterion = nn.CrossEntropyLoss()
+        self.tokenizer = tokenizer
+        self.last_validation_batch = None
+        self.last_validation_output = None
     
     def forward(self, input_ids, attention_mask = None, gt_input_ids = None):
         if attention_mask is not None and gt_input_ids is not None:
@@ -51,8 +57,9 @@ class CodeBertJS(pl.LightningModule):
         input_ids = batch['input_ids']
         attention_mask = batch['attention_mask']
         gt_input_ids = batch['gt_input_ids']
+        self.last_validation_batch = batch
         loss, outputs = self.forward(input_ids, attention_mask, gt_input_ids)
-
+        self.last_validation_output = outputs
         self.log("val_loss", loss, prog_bar=True, logger=True)
         return loss
 
@@ -69,8 +76,31 @@ class CodeBertJS(pl.LightningModule):
         return self.model(
             input_ids=input_ids,
         )
+        
+    def on_validation_end(self):
+        last_input_tokens = self.tokenizer.convert_ids_to_tokens(self.last_validation_batch['input_ids'][-1])
+        last_gt_tokens = self.tokenizer.convert_ids_to_tokens(self.last_validation_batch['gt_input_ids'][-1])
+        last_output_tokens = torch.argmax(self.last_validation_output, dim=-1).tolist()
+        last_input_seq = self.tokenizer.convert_tokens_to_string(last_input_tokens).replace('<s>','').replace('</s>','').replace('<pad>','')
+        last_gt_seq = self.tokenizer.convert_tokens_to_string(last_gt_tokens).replace('<s>','').replace('</s>','').replace('<pad>','')
+        codeDiff = unified_diff(last_input_seq, last_gt_seq)
+        codeDiffStr = "\n".join(codeDiff)
+        last_output_seq = self.tokenizer.batch_decode(last_output_tokens[-1], skip_special_tokens=True)
+        print(last_output_seq)
+        raise ValueError
+        last_output_seq = self.tokenizer.convert_tokens_to_string(last_output_seq)
+        
+        log_msg = f"""
+        Val Batch Sample:
+        --------------------Output Code-------------------
+        {last_output_seq}
+        --------------------Code Diff---------------------
+        {codeDiffStr}
+        """
+        with open(f"train-{datetime.today().strftime('%Y-%m-%d')}-CodeBert.log", 'a') as f:
+            f.write(log_msg)
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> optim.Adam:
         optimizer = optim.Adam(self.parameters(), lr=0.0001)
         return optimizer
 
