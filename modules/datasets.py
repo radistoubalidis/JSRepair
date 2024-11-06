bb7519307d476168fc6534f6a5368d799321afae
radistoubalidis <rtoumpalidis@gmail.com>
2024-11-07 00:29:52 +0200

    cast list of class labels in datasets into torch tensor

--- modules/datasets.py
+++ modules/datasets.py
@@ -1,32 +1,11 @@
-import sys
 import torch
-
-class CodeSearchNetDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, sample_id):
-        # Permutate the tokenized input_ids to feed as "bad sample"
-        input_ids_copy = encodings.copy().input_ids
-        self.input_ids = input_ids_copy[:, torch.randperm(input_ids_copy.size(1))]
-        self.attention_mask = encodings.attention_mask
-        # Labels are the "good_samples"
-        self.labels = encodings.input_ids
-
-    def __getitem__(self, idx):
-        return {
-            'input_ids': self.input_ids[idx],
-            'attention_mask': self.attention_mask[idx],
-            'labels': self.labels[idx]
-        }
-
-    def __len__(self):
-        return len(self.input_ids)
-
 
 class CodeBertDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, gt_input_ids, class_labels):
         self.input_ids = encodings.input_ids
         self.attention_mask = encodings.attention_mask
         self.gt_input_ids = gt_input_ids
-        self.class_labels = class_labels
+        self.class_labels = torch.tensor(class_labels)
 
     def __getitem__(self, idx):
         item = {
@@ -45,7 +24,7 @@ class CodeT5Dataset(torch.utils.data.Dataset):
         self.input_ids = encodings.input_ids
         self.attention_mask = encodings.attention_mask
         self.labels = decodings
-        self.class_labels = class_labels
+        self.class_labels = torch.tensor(class_labels)
 
     def __getitem__(self, idx):
         item = {
